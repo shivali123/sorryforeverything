c9df8c3c0537ea960c31b446d50fced41d7e352a
shivali kapoor <shivalikapoor@192.168.1.5>
2023-03-21 11:15:21 +0530

    all code

--- src/App.js
+++ src/App.js
@@ -1,25 +1,84 @@
-import logo from './logo.svg';
-import './App.css';
+import * as React from "react";
+import DialogTitle from "@mui/material/DialogTitle";
+import Dialog from "@mui/material/Dialog";
+import { height } from "@mui/system";
 
-function App() {
+export default function App(props) {
+  const [openDialog, handleDisplay] = React.useState(false);
+  const [yescount, setcount] = React.useState(0);
+  const [nocount, setnocount] = React.useState(0);
+
+  const [title, setitle] = React.useState('Did you felt bad what i said?');
+  const arr = ['I am sorry, are you still angry with me',
+    'Still angry?',
+    'Still angry?',
+    'Still angry?',
+    'Still angry?']
+  const brr = ['will you go for a coffee?',
+    'Pleaseeee',
+    'Pleaseeee',
+    'Pleaseeee',
+    'Pleaseee'];
+  const [selectedoptions, setselectedoptions] = React.useState(false);
+  const handleClose = () => {
+    handleDisplay(false);
+  };
+
+  const openDialogBox = () => {
+    handleDisplay(true);
+  };
+  const dialogStyle = {
+    padding: "20px",
+  };
+  const buttonStyle = {
+    width: "10rem",
+    fontsize: "1.5rem",
+    height: "2rem",
+    padding: "5px",
+    borderRadius: "10px",
+    backgroundColor: "green",
+    color: "White",
+    border: "2px solid yellow",
+  };
+
+
+  const clickedyes = () => {
+    if (yescount < 5) {
+      setitle(arr[yescount]);
+      let c = yescount + 1;
+      setcount(c);
+      openDialogBox();
+    }
+    else {
+      setitle('okay :( ');
+    }
+  }
+  const clickedno = () => {
+    if (nocount < 5) {
+      setitle(brr[nocount]);
+      let c = nocount + 1;
+      setnocount(c);
+      openDialogBox();
+    }
+    else {
+      setitle('Fine , sorry ');
+    }
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <button style={buttonStyle} onClick={openDialogBox}>
+        Open Dialog
+      </button>
+      <Dialog onClose={handleClose} open={openDialog}>
+        <h3 style={dialogStyle}>
+          {title}
+        </h3>
+        <div style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+          <input type='button' onClick={clickedyes} style={{ margin: 'auto' }} value='yes' id='btnYes' />
+          <input type='button' onClick={clickedno} style={{ marginLeft: '10px' }} value='no' id='btnNo' />
+        </div>
+      </Dialog>
+    </>
   );
 }
-
-export default App;
