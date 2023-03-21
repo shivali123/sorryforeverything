9c43dd26e27366ae115460c7d2a788a39a2dba0c
shivali kapoor <shivalikapoor@192.168.1.5>
2023-03-21 13:53:05 +0530

    solving pages

--- src/App.js
+++ src/App.js
@@ -8,17 +8,17 @@ export default function App(props) {
   const [yescount, setcount] = React.useState(0);
   const [nocount, setnocount] = React.useState(0);
 
-  const [title, setitle] = React.useState('Did you felt bad what i said?');
-  const arr = ['I am sorry, are you still angry with me',
-    'Still angry?',
-    'Still angry?',
-    'Still angry?',
-    'Still angry?']
-  const brr = ['will you go for a coffee?',
+  const [title, setitle] = React.useState('Did you felt bad what i said????');
+  const arr = ['I am sorry, are you still angry with me??',
+    'Still angry with me?',
+    'Still angry ?',
+    'Still angry????',
+    'Still angry???']
+  const brr = ['Will you go for a coffee?',
     'Pleaseeee',
-    'Pleaseeee',
-    'Pleaseeee',
-    'Pleaseee'];
+    'Pleaseeee ????',
+    'Pleaseeee ?????????',
+    'Pleaseee :(  '];
   const [selectedoptions, setselectedoptions] = React.useState(false);
   const handleClose = () => {
     handleDisplay(false);
@@ -28,7 +28,7 @@ export default function App(props) {
     handleDisplay(true);
   };
   const dialogStyle = {
-    padding: "20px",
+    padding: "50px",
   };
   const buttonStyle = {
     width: "10rem",
@@ -50,7 +50,7 @@ export default function App(props) {
       openDialogBox();
     }
     else {
-      setitle('okay :( ');
+      setitle('Okay :( ');
     }
   }
   const clickedno = () => {
@@ -61,7 +61,7 @@ export default function App(props) {
       openDialogBox();
     }
     else {
-      setitle('Fine , sorry ');
+      setitle('Fine , sorry once again :( ');
     }
   }
 
@@ -70,13 +70,13 @@ export default function App(props) {
       <button style={buttonStyle} onClick={openDialogBox}>
         Open Dialog
       </button>
-      <Dialog onClose={handleClose} open={openDialog}>
+      <Dialog onClose={handleClose} open={openDialog} >
         <h3 style={dialogStyle}>
           {title}
         </h3>
-        <div style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-          <input type='button' onClick={clickedyes} style={{ margin: 'auto' }} value='yes' id='btnYes' />
-          <input type='button' onClick={clickedno} style={{ marginLeft: '10px' }} value='no' id='btnNo' />
+        <div style={{ display: 'flex', left: '50%', justifyContent: 'center', alignItems: 'center', alignItems: 'center' }}>
+          <input type='button' onClick={clickedyes} style={{ marginLeft: '20px', marginBottom: '5px', padding: '10px' }} value='yes' id='btnYes' />
+          <input type='button' onClick={clickedno} style={{ marginLeft: '20px', marginBottom: '5px', padding: '10px' }} value='no' id='btnNo' />
         </div>
       </Dialog>
     </>
